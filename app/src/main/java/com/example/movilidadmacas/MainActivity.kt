5c879f883fb18d2fcf72a2cee0e42df1bce876e4
ApoloV21 <apolov67@gmail.com>
2025-06-13 14:11:44 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/movilidadmacas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.movilidadmacas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movilidadmacas.ui.theme.MovilidadMacasTheme
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var map: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Configuration.getInstance().load(applicationContext, getPreferences(MODE_PRIVATE))
+        setContentView(R.layout.activity_main)
+
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setMultiTouchControls(true)
+
+        val startPoint = GeoPoint(-2.3, -78.116666666667) //-2.3, -78.116666666667
+        map.controller.setZoom(15.0)
+        map.controller.setCenter(startPoint)
+
+        val marker = Marker(map)
+        marker.position = startPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.title = "Centro de Macas"
+        map.overlays.add(marker)
+
+
+    }
+}
+
